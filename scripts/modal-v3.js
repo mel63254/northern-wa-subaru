ba1263f5d34f636208b44eec60f951a55f72f6b4
mel63254 <55901724+mel63254@users.noreply.github.com>
2019-12-25 14:39:03 -0800

    Create modal-v3.js

---
+++ scripts/modal-v3.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("myModal");
+
+for(var i = 1; i < 6; i++) {
+    var img = document.getElementById("myImg" + i);
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
