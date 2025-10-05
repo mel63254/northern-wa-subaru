2db6b5630de1789e37c82eaa47bed84e1fa44761
mel63254 <55901724+mel63254@users.noreply.github.com>
2025-10-05 13:41:51 -0700

    Update modal-v3.js

--- scripts/modal-v3.js
+++ scripts/modal-v3.js
@@ -1,4 +1,4 @@
-function(){
+/*function(){
     try {
         var modal = document.getElementById("myModal");
         var modalImg = document.getElementById("img01");
@@ -36,9 +36,9 @@ function(){
     catch(e) {}
 }
                                                         
-                                                                                          
+*/                                                                                          
 
-/*
+
 var modal = document.getElementById("myModal");
 
 for(var i = 1; i < 6; i++) {
@@ -58,4 +58,3 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
-*/
